67424e39e95caf4314711364e1f58a862fd7e355
yinmh17 <31975170+yinmh17@users.noreply.github.com>
2019-07-19 15:51:46 +0800

    Update __init__.py

--- ops/nl/__init__.py
+++ ops/nl/__init__.py
@@ -1,6 +1,7 @@
 from .nonlocal_block import NonLocal2d
 from .context_block import ContextBlock2d
+from .nonlocal_block_bn import Nonlocal2d_bn
 
 __all__ = [
-    'NonLocal2d', 'ContextBlock2d'
+    'NonLocal2d','Nonlocal2d_bn', 'ContextBlock2d'
 ]
