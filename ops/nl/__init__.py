e199056e6c8ee78b30db77724e7b0b4aa52c7f5b
yinmh17 <31975170+yinmh17@users.noreply.github.com>
2019-07-19 16:23:07 +0800

    Update __init__.py

--- ops/nl/__init__.py
+++ ops/nl/__init__.py
@@ -1,4 +1,5 @@
-from .nonlocal_block import NonLocal2d,Nonlocal2d_bn
+from .nonlocal_block import NonLocal2d
+from .nonlocal_block_bn import Nonlocal2d_bn
 from .context_block import ContextBlock2d
 
 
