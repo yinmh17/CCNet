928a9ddfc601179df478b9b665c46fdc4a5f9cf4
yinmh17 <31975170+yinmh17@users.noreply.github.com>
2019-07-22 23:08:43 +0800

    Update __init__.py

--- ops/__init__.py
+++ ops/__init__.py
@@ -1,7 +1,7 @@
 from .nl import NonLocal2d,NonLocal2d_bn
 from .gcb import ContextBlock
-from .multihead import MultiheadBlock,MultiheadSpatialBlock
+from .multihead import MultiheadBlock,MultiheadSpatialBlock,MultiheadRelationBlock
 
 __all__ = [
-    'NonLocal2d', 'NonLocal2d_bn', 'ContextBlock', 'MultiheadBlock', 'MultiheadSpatialBlock',
+    'NonLocal2d', 'NonLocal2d_bn', 'ContextBlock', 'MultiheadBlock', 'MultiheadSpatialBlock', 'MultiheadRelationBlock',
 ]
