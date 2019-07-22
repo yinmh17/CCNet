4cb29acdc9ad5db5913367cf56816dc3a1251548
yinmh17 <31975170+yinmh17@users.noreply.github.com>
2019-07-23 01:32:46 +0800

    Update gcnet.py

--- networks/gcnet.py
+++ networks/gcnet.py
@@ -13,7 +13,7 @@ import functools
 import sys, os
 
 from libs import InPlaceABN, InPlaceABNSync
-from ops import NonLocal2d, NonLocal2d_bn, ContextBlock, MultiheadBlock, MultiheadSpatialBlock
+from ops import NonLocal2d, NonLocal2d_bn, ContextBlock, MultiheadBlock, MultiheadSpatialBlock, MultiRelationBlock, MultiheadRelationBlock
 
 
 BatchNorm2d = functools.partial(InPlaceABNSync, activation='none')
@@ -102,9 +102,9 @@ class Bottleneck(nn.Module):
         return out
 
 class GCBModule(nn.Module):
-    def __init__(self, in_channels, out_channels, num_classes, type='gcb'):
+    def __init__(self, in_channels, out_channels, num_classes, type='multi_relation'):
         super(GCBModule, self).__init__()
-        assert type in ['gcb', 'nl', 'nl_bn', 'multi', 'multi_spatial']
+        assert type in ['gcb', 'nl', 'nl_bn', 'multi', 'multi_spatial', 'multi_relation', 'multihead_relation']
         inter_channels = in_channels // 4
         self.conva = nn.Sequential(nn.Conv2d(in_channels, inter_channels, 3, padding=1, bias=False),
                                    InPlaceABNSync(inter_channels))
@@ -117,6 +117,12 @@ class GCBModule(nn.Module):
         elif type == 'multi':
             self.ctb = MultiheadBlock(inter_channels, ratio=1./4, one_fc=True, 
                                       head_num=8, pre_group=1, post_group=8)
+        elif type == 'multi_relation':
+            self.ctb = MultiRelationBlock(inter_channels, ratio=1.0/4, one_fc=True, mask_num=8, relation_num=8,
+                                          pre_group=1, post_group=1, geo_feature_dim=64, key_feature_dim=64)
+        elif type == 'multihead_relation':
+            self.ctb = MultiheadRelationBlock(inter_channels, ratio=1.0/4, one_fc=True, head_num=8,
+                                          pre_group=1, post_group=1, geo_feature_dim=64, key_feature_dim=64)
         elif type == 'multi_spatial':
             self.ctb = MultiheadSpatialBlock(inter_channels, ratio=1./4, head_num=8)
         else:
@@ -207,5 +213,5 @@ class ResNet(nn.Module):
 
 
 def Res_Deeplab(num_classes=21):
-    model = ResNet(Bottleneck,[3, 4, 23, 3], num_classes, with_att=True, att='ct', att_stage=[False, True, True, True], att_pos='after_1x1')
+    model = ResNet(Bottleneck,[3, 4, 23, 3], num_classes, with_att=False, att='ct', att_stage=[False, True, True, True], att_pos='after_1x1')
     return model
