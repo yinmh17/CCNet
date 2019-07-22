8e075900b58758d329f7cd73d323ca4e02b8370f
yinmh17 <31975170+yinmh17@users.noreply.github.com>
2019-07-23 01:25:25 +0800

    Update __init__.py

--- ops/multihead/__init__.py
+++ ops/multihead/__init__.py
@@ -1,7 +1,8 @@
 from .multihead_block import MultiheadBlock
 from .multihead_spatial_block import MultiheadSpatialBlock
+from .multi_relation_block import MultiRelationBlock
 from .multihead_relation_block import MultiheadRelationBlock
 
 __all__ = [
-    'MultiheadBlock', 'MultiheadSpatialBlock', 'MultiheadRelationBlock',
+    'MultiheadBlock', 'MultiheadSpatialBlock', 'MultiRelationBlock', 'MultiheadRelationBlock',
 ]
