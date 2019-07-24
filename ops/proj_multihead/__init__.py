59920f6bb0366cf1c308ffd4bffcee7f55ebee91
yinmh17 <31975170+yinmh17@users.noreply.github.com>
2019-07-24 21:22:08 +0800

    Update __init__.py

--- ops/proj_multihead/__init__.py
+++ ops/proj_multihead/__init__.py
@@ -1,5 +1,6 @@
 from .proj_multihead_block import ProjMultiheadBlock
+from .proj_spatial_block import ProjSpatialBlock
 
 __all__ = [
-    'ProjMultiheadBlock',
+    'ProjMultiheadBlock', 'ProjSpatialBlock',
 ]
