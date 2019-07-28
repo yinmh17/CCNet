4fa81094b6cf43e02595caf67da145718a758254
yinmh17 <31975170+yinmh17@users.noreply.github.com>
2019-07-28 21:27:01 +0800

    Update run_local.sh

--- run_local.sh
+++ run_local.sh
@@ -13,5 +13,5 @@ GPU_IDS=0,1,2,3
 OHEM=1 #set to 1 for reducing the performance gap between val and test set.
 
 #variable ${LOCAL_OUTPUT} dir can save data of you job, after exec it will be upload to hadoop_out path 
-python train.py --data-dir ${CS_PATH} --random-mirror --random-scale --restore-from /philly/sc3/resrchprojvc2/zhez/model/resnet101-imagenet.pth --gpu ${GPU_IDS} --learning-rate ${LR} --input-size ${INPUT_SIZE},${INPUT_SIZE} --weight-decay ${WD} --batch-size ${BS} --num-steps ${STEPS} --recurrence ${R} --ohem ${OHEM} --ohem-thres 0.7 --ohem-keep 100000
+python train.py --data-dir ${CS_PATH} --random-mirror --random-scale --restore-from /var/storage/shared/sc3/resrchprojvc2/zhez/model/resnet101-imagenet.pth --gpu ${GPU_IDS} --learning-rate ${LR} --input-size ${INPUT_SIZE},${INPUT_SIZE} --weight-decay ${WD} --batch-size ${BS} --num-steps ${STEPS} --recurrence ${R} --ohem ${OHEM} --ohem-thres 0.7 --ohem-keep 100000
 python evaluate.py --data-dir ${CS_PATH} --restore-from snapshots/CS_scenes_${STEPS}.pth --gpu 0 --recurrence ${R}
