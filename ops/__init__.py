e7cb977b382ede68cf25a09981cfb22dc9d2da4d
yinmh17 <31975170+yinmh17@users.noreply.github.com>
2019-08-04 19:00:41 +0800

    Update __init__.py

--- ops/__init__.py
+++ ops/__init__.py
@@ -3,8 +3,9 @@ from .gcb import ContextBlock
 from .multihead import MultiheadBlock,MultiheadSpatialBlock,MultiRelationBlock,MultiheadRelationBlock
 from .glore import GloreUnit
 from .proj_multihead import ProjMultiheadBlock, ProjSpatialBlock
+from .mask_nl import MaskNonLocal2d
 
 __all__ = [
     'NonLocal2d', 'NonLocal2d_bn', 'ContextBlock', 'MultiheadBlock', 'MultiheadSpatialBlock', 'MultiRelationBlock', 
-    'MultiheadRelationBlock', 'GloreUnit', 'ProjMultiheadBlock', 'ProjSpatialBlock',
+    'MultiheadRelationBlock', 'GloreUnit', 'ProjMultiheadBlock', 'ProjSpatialBlock', 'MaskNonlocal2d',
 ]
