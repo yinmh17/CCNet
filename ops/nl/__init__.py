0e10446bfefb860b9cacce5743314b6d57b3f847
yinmh17 <31975170+yinmh17@users.noreply.github.com>
2019-08-09 14:44:19 +0800

    Update __init__.py

--- ops/nl/__init__.py
+++ ops/nl/__init__.py
@@ -2,7 +2,8 @@ from .nonlocal_block import NonLocal2d
 from .nonlocal_block_bn import NonLocal2d_bn
 from .context_block import ContextBlock2d
 from .nonlocal_block_cos import NonLocal2dCos
+from .nonlocal_gc_block import NonLocal2dGc
 
 __all__ = [
-    'NonLocal2d','NonLocal2d_bn', 'ContextBlock2d', 'NonLocal2dCos',
+    'NonLocal2d','NonLocal2d_bn', 'ContextBlock2d', 'NonLocal2dCos', 'NonLocal2dGc',
 ]
