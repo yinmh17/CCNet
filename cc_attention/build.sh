8246b1ce41db173f605ffeed592be32204c37043
yinmh17 <31975170+yinmh17@users.noreply.github.com>
2019-10-09 16:33:02 +0800

    Update build.sh

--- cc_attention/build.sh
+++ cc_attention/build.sh
@@ -5,6 +5,7 @@ CUDA_GENCODE="\
 -gencode=arch=compute_70,code=sm_70 \
 -gencode=arch=compute_60,code=sm_60 \
 -gencode=arch=compute_61,code=sm_61 \
+-gencode=arch=compute_62,code=sm_62 \
 -gencode=arch=compute_52,code=sm_52 \
 -gencode=arch=compute_50,code=sm_50 \
 -gencode=arch=compute_30,code=sm_30"
