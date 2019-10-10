4d6b20afd63fa4fa83845f2bdd674fca97c3c60d
yinmh17 <31975170+yinmh17@users.noreply.github.com>
2019-10-10 18:07:00 +0800

    Update train.py

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ import sys
 import os
 from tqdm import tqdm
 import os.path as osp
-from networks.gcnet import Res_Deeplab
+#from networks.gcnet import Res_Deeplab
 from dataset.datasets import CSDataSet
 #import matplotlib.pyplot as plt
 import random
@@ -23,7 +23,7 @@ from tensorboardX import SummaryWriter
 from utils.utils import decode_labels, inv_preprocess, decode_predictions
 from utils.criterion import CriterionCrossEntropy, CriterionOhemCrossEntropy, CriterionDSN, CriterionOhemDSN
 from utils.encoding import DataParallelModel, DataParallelCriterion
-
+from utils.utils import fromfile
 torch_ver = torch.__version__[:3]
 if torch_ver == '0.3':
     from torch.autograd import Variable
@@ -31,7 +31,7 @@ if torch_ver == '0.3':
 start = timeit.default_timer()
 
 IMG_MEAN = np.array((104.00698793,116.66876762,122.67891434), dtype=np.float32)
-
+'''
 BATCH_SIZE = 8
 DATA_DIRECTORY = 'cityscapes'
 DATA_LIST_PATH = './dataset/list/cityscapes/train.lst'
@@ -45,10 +45,10 @@ POWER = 0.9
 RANDOM_SEED = 1234
 RESTORE_FROM = './dataset/resnet101-imagenet.pth'
 SAVE_NUM_IMAGES = 2
-SAVE_PRED_EVERY = 500
+SAVE_PRED_EVERY = 10000
 SNAPSHOT_DIR = 'snapshots/'
 WEIGHT_DECAY = 0.0005
-
+'''
 def str2bool(v):
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
@@ -64,54 +64,27 @@ def get_arguments():
       A list of parsed arguments.
     """
     parser = argparse.ArgumentParser(description="DeepLab-ResNet Network")
-    parser.add_argument("--batch-size", type=int, default=BATCH_SIZE,
-                        help="Number of images sent to the network in one step.")
-    parser.add_argument("--data-dir", type=str, default=DATA_DIRECTORY,
+    parser.add_argument("--data-dir", type=str, default=None,
                         help="Path to the directory containing the PASCAL VOC dataset.")
-    parser.add_argument("--data-list", type=str, default=DATA_LIST_PATH,
-                        help="Path to the file listing the images in the dataset.")
-    parser.add_argument("--ignore-label", type=int, default=IGNORE_LABEL,
-                        help="The index of the label to ignore during the training.")
-    parser.add_argument("--input-size", type=str, default=INPUT_SIZE,
-                        help="Comma-separated string with height and width of images.")
     parser.add_argument("--is-training", action="store_true",
                         help="Whether to updates the running means and variances during the training.")
-    parser.add_argument("--learning-rate", type=float, default=LEARNING_RATE,
-                        help="Base learning rate for training with polynomial decay.")
-    parser.add_argument("--momentum", type=float, default=MOMENTUM,
-                        help="Momentum component of the optimiser.")
     parser.add_argument("--not-restore-last", action="store_true",
                         help="Whether to not restore last (FC) layers.")
-    parser.add_argument("--num-classes", type=int, default=NUM_CLASSES,
-                        help="Number of classes to predict (including background).")
     parser.add_argument("--start-iters", type=int, default=0,
                         help="Number of classes to predict (including background).")
-    parser.add_argument("--num-steps", type=int, default=NUM_STEPS,
-                        help="Number of training steps.")
-    parser.add_argument("--power", type=float, default=POWER,
-                        help="Decay parameter to compute the learning rate.")
     parser.add_argument("--random-mirror", action="store_true",
                         help="Whether to randomly mirror the inputs during the training.")
     parser.add_argument("--random-scale", action="store_true",
                         help="Whether to randomly scale the inputs during the training.")
-    parser.add_argument("--random-seed", type=int, default=RANDOM_SEED,
-                        help="Random seed to have reproducible results.")
-    parser.add_argument("--restore-from", type=str, default=RESTORE_FROM,
+    parser.add_argument("--restore-from", type=str, default=None,
                         help="Where restore model parameters from.")
-    parser.add_argument("--save-num-images", type=int, default=SAVE_NUM_IMAGES,
-                        help="How many images to save.")
-    parser.add_argument("--save-pred-every", type=int, default=SAVE_PRED_EVERY,
-                        help="Save summaries and checkpoint every often.")
-    parser.add_argument("--snapshot-dir", type=str, default=SNAPSHOT_DIR,
-                        help="Where to save snapshots of the model.")
-    parser.add_argument("--weight-decay", type=float, default=WEIGHT_DECAY,
-                        help="Regularisation parameter for L2-loss.")
     parser.add_argument("--gpu", type=str, default='None',
                         help="choose gpu device.")
     parser.add_argument("--recurrence", type=int, default=1,
                         help="choose the number of recurrence.")
     parser.add_argument("--ft", type=bool, default=False,
                         help="fine-tune the model with large input size.")
+    parser.add_argument('--config', help='train config file path')
 
     parser.add_argument("--ohem", type=str2bool, default='False',
                         help="use hard negative mining")
@@ -122,14 +95,17 @@ def get_arguments():
     return parser.parse_args()
 
 args = get_arguments()
+cfg=fromfile(args.config)
 
+if cfg.model.type == 'gcnet':
+    from networks.gcnet import Res_Deeplab
 
 def lr_poly(base_lr, iter, max_iter, power):
     return base_lr*((1-float(iter)/max_iter)**(power))
             
 def adjust_learning_rate(optimizer, i_iter):
     """Sets the learning rate to the initial LR divided by 5 at 60th, 120th and 160th epochs"""
-    lr = lr_poly(args.learning_rate, i_iter, args.num_steps, args.power)
+    lr = lr_poly(cfg.train_cfg.learning_rate, i_iter, cfg.train_cfg.num_steps, cfg.train_cfg.power)
     optimizer.param_groups[0]['lr'] = lr
     return lr
 
@@ -145,28 +121,34 @@ def set_bn_momentum(m):
 
 def main():
     """Create the model and start the training."""
-    writer = SummaryWriter(args.snapshot_dir)
+    writer = SummaryWriter(cfg.train_cfg.snapshot_dir)
     
-    if not args.gpu == 'None':
-        os.environ["CUDA_VISIBLE_DEVICES"]=args.gpu
-    h, w = map(int, args.input_size.split(','))
+    if args.gpu is not None:
+        os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
+    if args.data_dir is not None:
+        cfg.data_cfg.data_dir = args.data_dir
+    if args.restore_from is not None:
+        cfg.train_cfg.restore_from = args.restore_from
+    if args.start_iters is not None:
+        cfg.train_cfg.start_iters = args.start_iters
+
+    h, w = map(int, cfg.data_cfg.input_size.split(','))
     input_size = (h, w)
 
     cudnn.enabled = True
 
     # Create network.
-    deeplab = Res_Deeplab(num_classes=args.num_classes)
+    deeplab = Res_Deeplab(cfg.model,cfg.data_cfg.num_classes)
     print(deeplab)
 
     saved_state_dict = torch.load(args.restore_from)
     new_params = deeplab.state_dict().copy()
     for i in saved_state_dict:
         i_parts = i.split('.')
-        if not i_parts[0]=='fc':
+        if not i_parts[0] == 'fc':
             new_params['.'.join(i_parts[0:])] = saved_state_dict[i] 
     
     deeplab.load_state_dict(new_params)
-
 
     model = DataParallelModel(deeplab)
     model.train()
@@ -177,26 +159,26 @@ def main():
     if args.ohem:
         criterion = CriterionOhemDSN(thresh=args.ohem_thres, min_kept=args.ohem_keep)
     else:
-        criterion = CriterionDSN() #CriterionCrossEntropy()
+        criterion = CriterionDSN()
     criterion = DataParallelCriterion(criterion)
     criterion.cuda()
     
     cudnn.benchmark = True
 
-    if not os.path.exists(args.snapshot_dir):
-        os.makedirs(args.snapshot_dir)
+    if not os.path.exists(cfg.train_cfg.snapshot_dir):
+        os.makedirs(cfg.train_cfg.snapshot_dir)
 
 
-    trainloader = data.DataLoader(CSDataSet(args.data_dir, args.data_list, max_iters=args.num_steps*args.batch_size, crop_size=input_size, 
-                    scale=args.random_scale, mirror=args.random_mirror, mean=IMG_MEAN), 
-                    batch_size=args.batch_size, shuffle=True, num_workers=4, pin_memory=True)
+    trainloader = data.DataLoader(CSDataSet(cfg.data_cfg.data_dir, cfg.data_cfg.data_list, max_iters=cfg.train_cfg.num_steps*cfg.train_cfg.batch_size,
+                                            crop_size=input_size,scale=args.random_scale, mirror=args.random_mirror, mean=IMG_MEAN),
+                                  batch_size=cfg.train_cfg.batch_size, shuffle=True, num_workers=4, pin_memory=True)
 
-    optimizer = optim.SGD([{'params': filter(lambda p: p.requires_grad, deeplab.parameters()), 'lr': args.learning_rate }], 
-                lr=args.learning_rate, momentum=args.momentum,weight_decay=args.weight_decay)
+    optimizer = optim.SGD([{'params': filter(lambda p: p.requires_grad, deeplab.parameters()), 'lr': cfg.train_cfg.learning_rate }],
+                lr=cfg.train_cfg.learning_rate, momentum=cfg.train_cfg.momentum,weight_decay=cfg.train_cfg.weight_decay)
     optimizer.zero_grad()
 
     for i_iter, batch in enumerate(trainloader):
-        i_iter += args.start_iters
+        i_iter += cfg.train_cfg.start_iters
         images, labels, _, _ = batch
         images = images.cuda()
         labels = labels.long().cuda()
@@ -206,7 +188,7 @@ def main():
 
         optimizer.zero_grad()
         lr = adjust_learning_rate(optimizer, i_iter)
-        preds = model(images, args.recurrence)
+        preds = model(images)
 
         loss = criterion(preds, labels)
         loss.backward()
@@ -227,16 +209,16 @@ def main():
         #         writer.add_image('Labels/'+str(index), lab, i_iter)
         #         writer.add_image('preds/'+str(index), preds_colors[index], i_iter)
 
-        print('iter = {} of {} completed, loss = {}'.format(i_iter, args.num_steps, loss.data.cpu().numpy()))
+        print('iter = {} of {} completed, loss = {}'.format(i_iter, cfg.train_cfg.num_steps, loss.data.cpu().numpy()))
 
-        if i_iter >= args.num_steps-1:
+        if i_iter >= cfg.train_cfg.num_steps-1:
             print('save model ...')
-            torch.save(deeplab.state_dict(),osp.join(args.snapshot_dir, 'CS_scenes_'+str(args.num_steps)+'.pth'))
+            torch.save(deeplab.state_dict(),osp.join(cfg.train_cfg.snapshot_dir, 'CS_scenes_'+str(cfg.train_cfg.num_steps)+'.pth'))
             break
 
-        if i_iter % args.save_pred_every == 0 and i_iter > 50000:
+        if i_iter % cfg.train_cfg.save_pred_every == 0 and i_iter > cfg.train_cfg.save_from:
             print('taking snapshot ...')
-            torch.save(deeplab.state_dict(),osp.join(args.snapshot_dir, 'CS_scenes_'+str(i_iter)+'.pth'))     
+            torch.save(deeplab.state_dict(),osp.join(cfg.train_cfg.snapshot_dir, 'CS_scenes_'+str(i_iter)+'.pth'))
 
     end = timeit.default_timer()
     print(end-start,'seconds')
