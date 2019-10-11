442004336ba0957875e3c3ba140d9e460251eda9
yinmh17 <31975170+yinmh17@users.noreply.github.com>
2019-10-11 16:56:11 +0800

    Update run_local.sh

--- run_local.sh
+++ run_local.sh
@@ -14,4 +14,4 @@ OHEM=1 #set to 1 for reducing the performance gap between val and test set.
 #variable ${LOCAL_OUTPUT} dir can save data of you job, after exec it will be upload to hadoop_out path 
 python train.py --data-dir ${CS_PATH} --random-mirror --random-scale --restore-from ${PRE_PATH} --config ${CONFIG} --gpu ${GPU_IDS} --start-iters ${START} --ohem ${OHEM} --ohem-thres 0.7 --ohem-keep 100000
 #python evaluate.py --data-dir ${CS_PATH} --restore-from snapshots/CS_scenes_${STEPS}.pth --gpu 0 --recurrence ${R} --config ${CONFIG}
-python evaluate.py --data-dir ${CS_PATH} --restore-from snapshots/CS_scenes_${STEPS}.pth --gpu 0,1,2,3 --recurrence ${R} --config ${CONFIG}
+python evaluate_all.py --data-dir ${CS_PATH} --restore-from snapshots/CS_scenes_${STEPS}.pth --gpu 0,1,2,3 --recurrence ${R} --config ${CONFIG}
