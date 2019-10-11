a65c0a902c273f1bb2d5065a706be2aed64edf1c
yinmh17 <31975170+yinmh17@users.noreply.github.com>
2019-10-11 17:43:48 +0800

    Update test.py

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ import os
 import scipy.ndimage as nd
 from math import ceil
 from PIL import Image as PILImage
-
+from utils.utils import fromfile
 import torch.nn as nn
 IMG_MEAN = np.array((104.00698793,116.66876762,122.67891434), dtype=np.float32)
 
