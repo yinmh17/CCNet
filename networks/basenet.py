c5ab4acee65923b1c843c484bb0ae6032eaf6cd3
yinmh17 <31975170+yinmh17@users.noreply.github.com>
2019-10-25 23:49:50 +0800

    Update basenet.py

--- networks/basenet.py
+++ networks/basenet.py
@@ -120,11 +120,7 @@ class GCBModule(nn.Module):
     def __init__(self, in_channels, out_channels, num_classes, cfg):
         super(GCBModule, self).__init__()
         type = cfg.type
-<<<<<<< HEAD
-        assert type in ['gcb', 'nl', 'nl_bn', 'nl_nowd', 'multi', 'multi_spatial', 'multi_relation', 'multihead_relation', 'glore', 'proj_multi', 'proj_spatial']
-=======
-        assert type in ['gcb', 'nl', 'nl_bn', 'multi', 'multi_spatial', 'multi_relation', 'multihead_relation', 'glore', 'mask_nl']
->>>>>>> c05cece80009d95c27ffaeb5211742e7caf2bbfc
+        assert type in ['gcb', 'nl', 'nl_bn', 'nl_nowd', 'multi', 'multi_spatial', 'multi_relation', 'multihead_relation', 'glore', 'mask_nl']
         inter_channels = in_channels // 4
         self.conva = nn.Sequential(nn.Conv2d(in_channels, inter_channels, 3, padding=1, bias=False),
                                    InPlaceABNSync(inter_channels))
@@ -135,14 +131,13 @@ class GCBModule(nn.Module):
         elif type == 'nl_bn':
             self.ctb = NonLocal2d_bn(inter_channels, inter_channels // 2, downsample=cfg.downsample, whiten_type=cfg.whiten_type,
                                      temperature=cfg.temp, with_gc=cfg.with_gc, use_out=cfg.use_out, out_bn=cfg.out_bn)
-<<<<<<< HEAD
+
         elif type == 'nl_nowd':
-            self.ctb = NonLocal2d_nowd(inter_channels, inter_channels // 2, downsample=cfg.downsample, whiten_type=cfg.whiten_type, weight_init_scale=cfg.weight_init_scale, with_gc=cfg.with_gc, use_out=cfg.use_out, out_bn=cfg.out_bn)
-=======
+            self.ctb = NonLocal2d_nowd(inter_channels, inter_channels // 2, downsample=cfg.downsample, whiten_type=cfg.whiten_type, 
+                                       weight_init_scale=cfg.weight_init_scale, with_gc=cfg.with_gc, use_out=cfg.use_out, out_bn=cfg.out_bn)
         elif type == 'mask_nl':
             self.ctb = MaskNonLocal2d(inter_channels, inter_channels // 2, downsample=cfg.downsample, whiten_type=cfg.whiten_type, temperature=cfg.temp,
                                       use_out=cfg.use_out, out_bn=cfg.out_bn, mask_type=cfg.mask_type, use_key_mask=True, use_query_mask=False, mask_pos='after', use_softmax=True)
->>>>>>> c05cece80009d95c27ffaeb5211742e7caf2bbfc
         elif type == 'multi':
             self.ctb = MultiheadBlock(inter_channels, ratio=1./4, one_fc=True, 
                                       head_num=8, pre_group=1, post_group=8)
