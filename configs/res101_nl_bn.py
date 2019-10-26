6c1131515b34f19e2213fbdffb91bc09b9567d24
yinmh17 <31975170+yinmh17@users.noreply.github.com>
2019-10-26 23:00:32 +0800

    Create res101_nl_bn.py

---
+++ configs/res101_nl_bn.py
@@ -0,0 +1,46 @@
+model = dict(
+    type='basenet',
+    pretrained='',
+    backbone=dict(
+        type='ResNet',
+        depth=101,
+        num_stages=4,
+        block_num=[3, 4, 23, 3],
+
+    ),
+    att=dict(
+        with_att=False,
+        type='glore',
+        att_stage=[False,False,True,False],
+        att_pos='after_add',
+        att_location=[[],[],[5,11,17],[]],
+    ),
+    module=dict(
+        type='nl',
+        downsample=False,
+        use_out=False,
+        out_bn=True,
+    )
+)
+train_cfg = dict(
+    batch_size=8,
+    learning_rate=1e-2,
+    momentum=0.9,
+    num_steps=60000,
+    power=0.9,
+    random_seed=1234,
+    restore_from='./dataset/resnet101-imagenet.pth',
+    save_num_images=2,
+    start_iters=0,
+    save_from=59500,
+    save_pred_every=100,
+    snapshot_dir='snapshots/',
+    weight_decay=0.0005
+)
+data_cfg = dict(
+    data_dir='cityscapes',
+    data_list='./dataset/list/cityscapes/train.lst',
+    ignore_label=255,
+    input_size='769,769',
+    num_classes=19,
+)
