dd0540d6fe37dd33337cc6e9e68a9bccc5d25aa3
yaozhuliang <yaozhuliang13@gmail.com>
2019-11-03 13:37:51 +0000

    add nowd gc/nl cfg

---
+++ configs/nowd/nl/res101_d_nl_lnnostd_ws1e0.py
@@ -0,0 +1,51 @@
+model = dict(
+    type='basenet',
+    pretrained='',
+    backbone=dict(
+        type='ResNet',
+        depth=101,
+        num_stages=4,
+        block_num=[3, 4, 23, 3],
+
+    ),
+    att=dict(
+        with_att=False,
+        type='glore',
+        att_stage=[False,False,True,False],
+        att_pos='after_add',
+        att_location=[[],[],[5,11,17],[]],
+    ),
+    module=dict(
+        type='nl_nowd',
+        downsample=True,
+        whiten_type=['ln_nostd'],
+        weight_init_scale=1.0,
+        with_gc=False,
+        with_nl=True,
+        nowd=[],
+        use_out=False,
+        out_bn=False,
+    )
+)
+train_cfg = dict(
+    batch_size=8,
+    learning_rate=1e-2,
+    momentum=0.9,
+    num_steps=60000,
+    power=0.9,
+    random_seed=1234,
+    restore_from='./dataset/resnet101-imagenet.pth',
+    save_num_images=2,
+    start_iters=0,
+    save_from=59500,
+    save_pred_every=100,
+    snapshot_dir='snapshots/',
+    weight_decay=0.0005
+)
+data_cfg = dict(
+    data_dir='cityscapes',
+    data_list='./dataset/list/cityscapes/train.lst',
+    ignore_label=255,
+    input_size='769,769',
+    num_classes=19,
+)
