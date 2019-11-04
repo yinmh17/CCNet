302ef2ffc91684f82aa0efc1905fcfd59c690f2a
yinmh17 <31975170+yinmh17@users.noreply.github.com>
2019-11-04 16:29:55 +0800

    Update test_multi.sh

--- test_multi.sh
+++ test_multi.sh
@@ -8,7 +8,7 @@ LIST2='./dataset/list/cityscapes/test2.lst'
 LIST3='./dataset/list/cityscapes/test3.lst'
 LIST4='./dataset/list/cityscapes/test4.lst'
 
-python test_multi.py --data-dir ${CS_PATH} --data-list ${LIST1} --restore-from ${PRE_PATH} --gpu 0 --whole --recurrence 1 --config ${CONFIG} &
-python test_multi.py --data-dir ${CS_PATH} --data-list ${LIST2} --restore-from ${PRE_PATH} --gpu 1 --whole --recurrence 1 --config ${CONFIG} &
-python test_multi.py --data-dir ${CS_PATH} --data-list ${LIST3} --restore-from ${PRE_PATH} --gpu 2 --whole --recurrence 1 --config ${CONFIG} &
-python test_multi.py --data-dir ${CS_PATH} --data-list ${LIST4} --restore-from ${PRE_PATH} --gpu 3 --whole --recurrence 1 --config ${CONFIG} &
+python test_multi.py --data-dir ${CS_PATH} --data-list ${LIST1} --restore-from ${PRE_PATH} --gpu 0 --whole True --recurrence 1 --config ${CONFIG} &
+python test_multi.py --data-dir ${CS_PATH} --data-list ${LIST2} --restore-from ${PRE_PATH} --gpu 1 --whole True --recurrence 1 --config ${CONFIG} &
+python test_multi.py --data-dir ${CS_PATH} --data-list ${LIST3} --restore-from ${PRE_PATH} --gpu 2 --whole True --recurrence 1 --config ${CONFIG} &
+python test_multi.py --data-dir ${CS_PATH} --data-list ${LIST4} --restore-from ${PRE_PATH} --gpu 3 --whole True --recurrence 1 --config ${CONFIG} &
